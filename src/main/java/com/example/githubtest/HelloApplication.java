0db7463eb99a086756e4d3feec5ad2fcf8f6e549
FelixTed <97131287+FelixTed@users.noreply.github.com>
2024-01-17 11:16:35 -0500

    Update HelloApplication.java

--- src/main/java/com/example/githubtest/HelloApplication.java
+++ src/main/java/com/example/githubtest/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         StackPane pane = new StackPane();
-        Circle circle = new Circle(50);
+        Rectangle circle = new Rectangle(50);
         pane.getChildren().add(circle);
         Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Hello!");
