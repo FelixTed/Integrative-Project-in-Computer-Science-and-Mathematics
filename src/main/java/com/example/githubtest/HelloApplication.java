82a06881dff3a9dc1d73ac3ac61b9f4d7355f62b
FelixTed <cadieuxfelix@outlook.com>
2024-01-17 11:15:17 -0500

    Replace with circle test

--- src/main/java/com/example/githubtest/HelloApplication.java
+++ src/main/java/com/example/githubtest/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.githubtest;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +12,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        StackPane pane = new StackPane();
+        Circle circle = new Circle(50);
+        pane.getChildren().add(circle);
+        Scene scene = new Scene(pane, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
