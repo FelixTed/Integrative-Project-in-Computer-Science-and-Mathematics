54fdc1f3693730301f2217e930cf1b3fb451cd3a
Felix Cadieux <cadieuxfelix@outlook.com>
2024-04-12 10:54:53 -0400

    added comments to Board and NameSelectionMenu

--- src/main/java/com/example/IntegrativeProject/Board.java
+++ src/main/java/com/example/IntegrativeProject/Board.java
@@ -27,7 +27,7 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class Board extends Application {
-    //Data field
+    // Data field
     private String player1Name = "player 1";
     private String player2Name = "player 2";
     private Player player1;
@@ -36,83 +36,80 @@ public class Board extends Application {
     private Stone[] stones2;
     private TextField energyField = new TextField();
     private TextField angleField = new TextField();
-    private Label statusLabel = new Label(player1Name+"'s Turn");
+    private Label statusLabel = new Label(player1Name + "'s Turn");
     private Label winningLabel = new Label("");
     private Button launchButton = new Button("Launch");
-    private  Line angleLine = new Line(100,380,250,380);
+    private Line angleLine = new Line(100, 380, 250, 380);
     private double lineLength = 150;
-    private Target target = new Target(new Circle(1100,380,40),new Circle(1100,380,100),new Circle(1100,380,180));
+    private Target target = new Target(new Circle(1100, 380, 40), new Circle(1100, 380, 100), new Circle(1100, 380, 180));
     private IntegerProperty currentStone = new SimpleIntegerProperty();
     private int currentPlayer;
     private Boolean paused = false;
     private Boolean endgame = false;
 
-    private Line upperBorder = new Line(25,80,1375,80);
-    private Line leftBorder = new Line(25,80,25,680);
-    private Line rightBorder = new Line(1375,80,1375,680);
-    private Line lowerBorder = new Line(25,680,1375,680);
-    private Line limit = new Line(250,80,250,680);
-    //Stores value for kEnergy, used to check if kEnergy is too big
+    private Line upperBorder = new Line(25, 80, 1375, 80);
+    private Line leftBorder = new Line(25, 80, 25, 680);
+    private Line rightBorder = new Line(1375, 80, 1375, 680);
+    private Line lowerBorder = new Line(25, 680, 1375, 680);
+    private Line limit = new Line(250, 80, 250, 680);
+    // Stores value for kEnergy, used to check if kEnergy is too big
     double value;
 
     @Override
     public void start(Stage stage) throws IOException {
-        //Setting correct name for user
-        statusLabel.setText(player1Name+"'s turn");
+        // Setting correct name for user
+        statusLabel.setText(player1Name + "'s turn");
         statusLabel.getStyleClass().add("status");
         winningLabel.getStyleClass().add("status");
 
-        //Putting the borders of the game in a list
-        Line[] borders = {leftBorder,upperBorder,rightBorder,lowerBorder};
+        // Putting the borders of the game in a list
+        Line[] borders = {leftBorder, upperBorder, rightBorder, lowerBorder};
 
-        limit.getStrokeDashArray().addAll(2d, 21d);;
+        limit.getStrokeDashArray().addAll(2d, 21d);
 
-        //Initializing player 1 and 2, with corresponding colored stones
-        stones1 = new Stone[]{new Stone(new Image("blueStone.png"),borders), new Stone(new Image("blueStone.png"),borders), new Stone(new Image("blueStone.png"),borders)};
-        stones2 = new Stone[]{new Stone(new Image("redStone.png"),borders), new Stone(new Image("redStone.png"),borders), new Stone(new Image("redStone.png"),borders)};
-        double[] currentSpeed = new double[stones1.length+ stones2.length];
+        // Initializing player 1 and 2, with corresponding colored stones
+        stones1 = new Stone[]{new Stone(new Image("blueStone.png"), borders), new Stone(new Image("blueStone.png"), borders), new Stone(new Image("blueStone.png"), borders)};
+        stones2 = new Stone[]{new Stone(new Image("redStone.png"), borders), new Stone(new Image("redStone.png"), borders), new Stone(new Image("redStone.png"), borders)};
+        double[] currentSpeed = new double[stones1.length + stones2.length];
 
-
-        for(int i = 0; i< stones1.length;i++){
+        for (int i = 0; i < stones1.length; i++) {
             stones1[i].setPlayerID(1);
             stones2[i].setPlayerID(2);
         }
 
-        //Creating the players and linking them to their personal list of stone
-        player1 = new Player(1,stones1);
-        player2 = new Player(2,stones2);
-        Player[] player = {player1,player2};
+        // Creating the players and linking them to their personal list of stone
+        player1 = new Player(1, stones1);
+        player2 = new Player(2, stones2);
+        Player[] player = {player1, player2};
 
-        //Setting the starting state of the game
+        // Setting the starting state of the game
         currentStone.set(0);
         currentPlayer = 0;
-        //Setting up pane to make nodes visible
+        // Setting up pane to make nodes visible
         Pane pane = new Pane();
 
-        //setting the background
+        // setting the background
         Image backGroundIm = new Image("gameBackground.jpg");
         BackgroundImage bGIMG = new BackgroundImage(backGroundIm, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
         Background background = new Background(bGIMG);
         pane.setBackground(background);
 
-        //Setting circles for the target
-        Circle wholeInner = new Circle(1100 ,380,40);
-
-
+        // Setting circles for the target
+        Circle wholeInner = new Circle(1100, 380, 40);
         wholeInner.setFill(Color.RED);
 
-        Circle wholeMid = new Circle(1100,380,100);
-        Circle insideMid = new Circle(1100,380,40);
+        Circle wholeMid = new Circle(1100, 380, 100);
+        Circle insideMid = new Circle(1100, 380, 40);
         insideMid.setFill(Color.TRANSPARENT);
 
-        Shape donut2 = Shape.subtract(wholeMid,insideMid);
+        Shape donut2 = Shape.subtract(wholeMid, insideMid);
         donut2.setFill(Color.LIGHTGRAY);
 
-        Circle wholeOuter = new Circle(1100,380,180);
-        Circle insideOuter = new Circle(1100,380,100);
+        Circle wholeOuter = new Circle(1100, 380, 180);
+        Circle insideOuter = new Circle(1100, 380, 100);
         insideOuter.setFill(Color.TRANSPARENT);
 
-        Shape donut3= Shape.subtract(wholeOuter,insideOuter);
+        Shape donut3 = Shape.subtract(wholeOuter, insideOuter);
         donut3.setFill(Color.BLUE);
 
         HBox hBox = new HBox();
@@ -125,18 +122,17 @@ public class Board extends Application {
         Label lbl2 = new Label("Angle: ");
         lbl2.getStyleClass().add("input");
 
-        hBox.getChildren().addAll(lbl1,energyField,lbl2,angleField,launchButton);
+        hBox.getChildren().addAll(lbl1, energyField, lbl2, angleField, launchButton);
 
         hBox.setTranslateX(50);
         hBox.setTranslateY(600);
 
-        statusLabel.setFont(Font.font("Times New Roman",32));
+        statusLabel.setFont(Font.font("Times New Roman", 32));
         statusLabel.setTextFill(Color.DARKBLUE);
         statusLabel.setTranslateX(30);
         statusLabel.setTranslateY(30);
 
-
-        Rectangle innerRect = new Rectangle(25,80,1350,600);
+        Rectangle innerRect = new Rectangle(25, 80, 1350, 600);
         innerRect.setFill(Color.LIGHTCYAN);
         innerRect.setStroke(Color.BLACK);
 
@@ -153,36 +149,34 @@ public class Board extends Application {
         winningLabel.setLayoutX(550);
         winningLabel.setLayoutY(375);
 
-        pane.getChildren().addAll(innerRect,wholeInner,donut2,donut3,limit,statusLabel,angleLine,hBox,stones1[0],upperBorder,lowerBorder,rightBorder,leftBorder);
+        pane.getChildren().addAll(innerRect, wholeInner, donut2, donut3, limit, statusLabel, angleLine, hBox, stones1[0], upperBorder, lowerBorder, rightBorder, leftBorder);
 
-
-        //Modify the angle of the line according to the angle value inputted
+        // Modify the angle of the line according to the angle value inputted
         angleField.textProperty().addListener(e -> {
             drawAngle();
         });
-        //Modify the angle of the line according to the angle value inputted
+        // Modify the angle of the line according to the angle value inputted
         energyField.textProperty().addListener(e -> {
             drawLength();
         });
 
-
         VBox vBox = new VBox();
-        Scene scene = new Scene(pane,1440,720);
+        Scene scene = new Scene(pane, 1440, 720);
         Image continueButt = new Image("ContinueButton.png");
         ImageView continueButtView = new ImageView(continueButt);
         Image menuButt = new Image("MenuButton.png");
         ImageView menuButtView = new ImageView(menuButt);
         Label pauseLbl = new Label("Paused");
         pauseLbl.setFont(Font.font("Times New Roman", 64));
-        vBox.getChildren().addAll(pauseLbl,continueButtView,menuButtView);
-        vBox.setMinSize(1440,720);
+        vBox.getChildren().addAll(pauseLbl, continueButtView, menuButtView);
+        vBox.setMinSize(1440, 720);
         // Pause menu of the game
-        scene.setOnKeyPressed(e->{
-            if(e.getCode() == KeyCode.ESCAPE && !paused && !endgame){
+        scene.setOnKeyPressed(e -> {
+            if (e.getCode() == KeyCode.ESCAPE && !paused && !endgame) {
                 paused = true;
-                for(int i = 0; i<stones1.length; i++){
+                for (int i = 0; i < stones1.length; i++) {
                     currentSpeed[i] = stones1[i].getKEnergy();
-                    currentSpeed[i+3] = stones2[i].getKEnergy();
+                    currentSpeed[i + 3] = stones2[i].getKEnergy();
                     stones1[i].setSpeed(0);
                     stones2[i].setSpeed(0);
                 }
@@ -193,76 +187,75 @@ public class Board extends Application {
                 menuButtView.setFitWidth(600);
                 pane.getChildren().addAll(vBox);
                 vBox.setAlignment(Pos.CENTER);
-
-            }else if(e.getCode() == KeyCode.ESCAPE && paused && !endgame){
+            } else if (e.getCode() == KeyCode.ESCAPE && paused && !endgame) {
                 paused = false;
                 pane.getChildren().remove(vBox);
-                for(int i =0;i< stones1.length;i++){
-                    if(stones1[i].isActive()) {
+                for (int i = 0; i < stones1.length; i++) {
+                    if (stones1[i].isActive()) {
                         stones1[i].startMoving(currentSpeed[i], stones1[i].getAngle(), stones1, stones2, stones1[i]);
                     }
-                    if(stones2[i].isActive()) {
+                    if (stones2[i].isActive()) {
                         stones2[i].startMoving(currentSpeed[i + 3], stones2[i].getAngle(), stones1, stones2, stones2[i]);
                     }
                 }
-            }else if(e.getCode() == KeyCode.ESCAPE && endgame){
+            } else if (e.getCode() == KeyCode.ESCAPE && endgame) {
                 stage.close();
                 Stage s = new Stage();
-                try{
+                try {
                     new MainMenu().start(s);
-                }catch(IOException ex){
+                } catch (IOException ex) {
                     throw new RuntimeException(ex);
                 }
             }
 
         });
-        continueButtView.setOnMouseClicked(ev->{
+        continueButtView.setOnMouseClicked(ev -> {
             paused = false;
             pane.getChildren().remove(vBox);
-            for(int i =0;i< stones1.length;i++){
-                if(stones1[i].isActive()) {
+            for (int i = 0; i < stones1.length; i++) {
+                if (stones1[i].isActive()) {
                     stones1[i].startMoving(currentSpeed[i], stones1[i].getAngle(), stones1, stones2, stones1[i]);
                 }
-                if(stones2[i].isActive()) {
+                if (stones2[i].isActive()) {
                     stones2[i].startMoving(currentSpeed[i + 3], stones2[i].getAngle(), stones1, stones2, stones2[i]);
                 }
             }
         });
-        menuButtView.setOnMouseClicked(ev->{
+        menuButtView.setOnMouseClicked(ev -> {
             paused = false;
             stage.close();
             Stage s = new Stage();
-            try{
+            try {
                 new MainMenu().start(s);
-            }catch(IOException ex){
+            } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
         });
-        //User Input
+        // User Input
         launchButton.setOnAction(e -> {
 
             Stone tempStone = player[currentPlayer].getStoneList()[currentStone.getValue()];
             try {
                 value = Double.parseDouble(energyField.getText());
-                if(value > 500){
+                if (value > 500) {
                     statusLabel.setText("Value cannot be more than 500!");
                     value = 500;
                 }
                 tempStone.startMoving(value, Integer.parseInt(angleField.getText()), stones1, stones2, tempStone);
-            }catch(NumberFormatException nfe){
+            } catch (NumberFormatException nfe) {
                 try {
                     tempStone.startMoving(value, 0, stones1, stones2, tempStone);
-                }catch(NumberFormatException nfe2){
-                    tempStone.startMoving(0,0,stones1,stones2,tempStone);
+                } catch (NumberFormatException nfe2) {
+                    tempStone.startMoving(0, 0, stones1, stones2, tempStone);
                 }
             }
             tempStone.setActive(true);
 
-           //Wait for the stone to stop moving before allowing the game to continue
-            tempStone.isMoving().addListener((e2,e3,value) ->{
-                if(value){
-                        launchButton.setDisable(true);
-                }else if(endgame && !value){
+            // Wait for the stone to stop moving before allowing the game to continue
+            tempStone.isMoving().addListener((e2, e3, value) -> {
+                if (value) {
+                    launchButton.setDisable(true);
+                } else if (endgame && !value) {
 
                     launchButton.setDisable(true);
                     try {
@@ -270,24 +263,24 @@ public class Board extends Application {
                     } catch (InterruptedException ex) {
                         throw new RuntimeException(ex);
                     }
-                }else if(currentStone.getValue() != 4) {
+                } else if (currentStone.getValue() != 4) {
                     launchButton.setDisable(false);
                 }
             });
 
-            if(currentPlayer == 1){
-                currentStone.set(currentStone.getValue()+1);
+            if (currentPlayer == 1) {
+                currentStone.set(currentStone.getValue() + 1);
                 currentPlayer = 0;
-                statusLabel.setText(player1Name+"'s Turn");
+                statusLabel.setText(player1Name + "'s Turn");
                 statusLabel.setTextFill(Color.DARKBLUE);
-            }else {
+            } else {
                 currentPlayer = 1;
-                statusLabel.setText(player2Name+"'s Turn");
+                statusLabel.setText(player2Name + "'s Turn");
                 statusLabel.setTextFill(Color.RED);
             }
             try {
                 pane.getChildren().add(player[currentPlayer].getStoneList()[currentStone.getValue()]);
-            }catch(ArrayIndexOutOfBoundsException aioobe){
+            } catch (ArrayIndexOutOfBoundsException aioobe) {
                 launchButton.setDisable(true);
                 pane.getChildren().add(winningLabel);
                 endgame = true;
@@ -301,34 +294,38 @@ public class Board extends Application {
         stage.initStyle(StageStyle.UTILITY);
         stage.show();
     }
-    //Modify the length of the line based on the value in energyField
-    public void drawLength(){
+
+    // Modify the length of the line based on the value in energyField
+    public void drawLength() {
         try {
             int inputtedLength = Integer.parseInt(energyField.getText());
-            if(inputtedLength < 30)
+            if (inputtedLength < 30)
                 lineLength = 1.75 * 30;
-            else if(inputtedLength > 300)
+            else if (inputtedLength > 300)
                 lineLength = 1.75 * 300;
             else
                 lineLength = 1.75 * inputtedLength;
-        }catch(NumberFormatException nfe){
+        } catch (NumberFormatException nfe) {
             lineLength = 150;
-        }finally{
+        } finally {
             drawAngle();
         }
     }
-    //Draw the
-    public  void drawAngle(){
+
+    // Draw the angle of the line based on the value in angleField
+    public void drawAngle() {
         try {
             double angleRad = Math.toRadians(Integer.parseInt(angleField.getText()));
             angleLine.setEndX(100 + lineLength * Math.cos(angleRad));
             angleLine.setEndY(380 - lineLength * Math.sin(angleRad));
-        }catch(NumberFormatException nfe){
-            //if angle input is not a numerical value, set angle line to initial position
-            angleLine.setEndX(100+lineLength);
+        } catch (NumberFormatException nfe) {
+            // if angle input is not a numerical value, set angle line to initial position
+            angleLine.setEndX(100 + lineLength);
             angleLine.setEndY(380);
         }
     }
+
+    // End the game and display the winner
     public void endGame() throws InterruptedException {
         winningLabel.setFont(new Font(50));
         statusLabel.setTextFill(Color.BLACK);
@@ -338,30 +335,32 @@ public class Board extends Application {
 
         player1.setPointsTotal(target.calculatePoints(stones1));
         player2.setPointsTotal(target.calculatePoints(stones2));
-        statusLabel.setText(player1Name+": " + player1.getPointsTotal() + " points\t"+player2Name+": " + player2.getPointsTotal() + " points");
+        statusLabel.setText(player1Name + ": " + player1.getPointsTotal() + " points\t" + player2Name + ": " + player2.getPointsTotal() + " points");
 
         int winner;
-        if(player1.getPointsTotal() > player2.getPointsTotal()) {
+        if (player1.getPointsTotal() > player2.getPointsTotal()) {
             winningLabel.setTextFill(Color.BLUE);
-            winningLabel.setText(player1Name+" WINS!");
-        }
-        else if(player1.getPointsTotal() < player2.getPointsTotal()){
+            winningLabel.setText(player1Name + " WINS!");
+        } else if (player1.getPointsTotal() < player2.getPointsTotal()) {
             winningLabel.setTextFill(Color.RED);
-            winningLabel.setText(player2Name+" WINS!");
-        }
-        else{
+            winningLabel.setText(player2Name + " WINS!");
+        } else {
             winningLabel.setTextFill(Color.BLACK);
             winningLabel.setText("Draw");
         }
-        for(int i = 0; i<stones1.length;i++){
+        for (int i = 0; i < stones1.length; i++) {
             stones1[i].setSpeedKEnergy(0);
             stones2[i].setSpeedKEnergy(0);
         }
     }
-    public void setPlayerNames(String player1, String player2){
+
+    // Set the names of the players
+    public void setPlayerNames(String player1, String player2) {
         this.player1Name = player1;
         this.player2Name = player2;
     }
+
+    // Nested class for delayed execution
     public static abstract class After extends Thread {
 
         private int sleep = 0;
@@ -373,8 +372,8 @@ public class Board extends Application {
         public void run() {
             try {
                 Thread.sleep(this.sleep);
-            } catch(InterruptedException e) {
-                //do something with e
+            } catch (InterruptedException e) {
+                // do something with e
             }
             this.after();
         }
--- src/main/java/com/example/IntegrativeProject/NameSelectionMenu.java
+++ src/main/java/com/example/IntegrativeProject/NameSelectionMenu.java
@@ -16,14 +16,16 @@ import java.io.IOException;
 public class NameSelectionMenu extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        // Basic menu screen
+        // Creating the background image for the menu screen
         Image backGroundIm = new Image("CurlingBackground.jpg");
         BackgroundImage bGIMG = new BackgroundImage(backGroundIm, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
 
+        // Creating the background using the image
         Background background = new Background(bGIMG);
         Pane pane = new Pane();
         pane.setBackground(background);
 
+        // Label and text field for Player 1's name input
         Label promptPlayer1 = new Label("Player 1, enter your name below");
         promptPlayer1.setTranslateX(340);
         promptPlayer1.setTranslateY(100);
@@ -34,6 +36,7 @@ public class NameSelectionMenu extends Application {
         player1Name.setTranslateX(440);
         player1Name.setTranslateY(150);
 
+        // Label and text field for Player 2's name input
         Label promptPlayer2 = new Label("Player 2, enter your name below");
         promptPlayer2.setTranslateX(340);
         promptPlayer2.setTranslateY(200);
@@ -44,6 +47,7 @@ public class NameSelectionMenu extends Application {
         player2Name.setTranslateX(440);
         player2Name.setTranslateY(250);
 
+        // Creating the play button
         Image iMPlay = new Image("PlayButton.png");
         ImageView iVPlay = new ImageView();
         iVPlay.setImage(iMPlay);
@@ -52,6 +56,7 @@ public class NameSelectionMenu extends Application {
         iVPlay.setTranslateX(420);
         iVPlay.setTranslateY(300);
 
+        // Limiting the length of player names to 20 characters
         int max = 20;
         player1Name.textProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue.length() > max) {
@@ -66,20 +71,27 @@ public class NameSelectionMenu extends Application {
             }
         });
 
+        // Event handler for when the play button is clicked
         iVPlay.setOnMouseClicked(e ->{
             Stage s = new Stage();
             try{
+                // Starting the game board with player names
                 Board board = new Board();
                 board.setPlayerNames(player1Name.getText(),player2Name.getText());
                 board.start(s);
             }catch(IOException ex){
                 throw new RuntimeException(ex);
             }
+            // Closing the name selection menu stage
             stage.close();
         });
 
+        // Adding all elements to the pane
         pane.getChildren().addAll(player1Name,promptPlayer1,promptPlayer2,player2Name,iVPlay);
+
+        // Creating the scene for the name selection menu
         Scene infoScene = new Scene(pane, 1024, 576);
+        // Adding CSS stylesheet for font style
         infoScene.getStylesheets().add(getClass().getResource("/fontstyle3.css").toExternalForm());
         stage.setScene(infoScene);
         stage.show();
